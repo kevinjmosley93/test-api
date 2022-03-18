663330342f859178821d7f69382f100d164759ef
Kevin J Mosley <Kevinjmosley93@gmail.com>
2022-03-18 12:11:34 -0700

    change posts to todos and add user routes

--- server.js
+++ server.js
@@ -8,15 +8,35 @@ app.get("/", async (req, res) => {
   res.status(200).sendFile(path.join(__dirname, '/index.html'))
 });
 
-app.get("/posts", async (req, res) => {
-  const fetchRes = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10");
+app.get("/todos", async (req, res) => {
+  const fetchRes = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=5");
   const data = await fetchRes.json();
 //   console.log(data);
   res.status(200).json(data);
 });
 
-app.get("/posts/:id", async (req, res) => {
-  const fetchRes = await fetch(`https://jsonplaceholder.typicode.com/posts/${req.params.id}`);
+app.get("/todos/:id", async (req, res) => {
+  const fetchRes = await fetch(`https://jsonplaceholder.typicode.com/todos/${req.params.id}`);
+  const data = await fetchRes.json();
+//   console.log(data);
+  res.status(200).json(data);
+});
+
+app.get("/users", async (req, res) => {
+  const fetchRes = await fetch("https://jsonplaceholder.typicode.com/users?_limit=5");
+  const data = await fetchRes.json();
+//   console.log(data);
+  res.status(200).json(data);
+});
+
+app.get("/users/:id", async (req, res) => {
+  const fetchRes = await fetch(`https://jsonplaceholder.typicode.com/users/${req.params.id}`);
+  const data = await fetchRes.json();
+//   console.log(data);
+  res.status(200).json(data);
+});
+app.get("/users/:id/todos", async (req, res) => {
+  const fetchRes = await fetch(`https://jsonplaceholder.typicode.com/users/${req.params.id}/posts`);
   const data = await fetchRes.json();
 //   console.log(data);
   res.status(200).json(data);
