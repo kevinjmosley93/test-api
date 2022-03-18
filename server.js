fdea2ffb88169ef523911d96a2a63916c60a28e3
Kevin J Mosley <Kevinjmosley93@gmail.com>
2022-03-18 09:57:40 -0700

    add index.html

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const express = require("express");
+const path = require('path')
 const app = express();
 const port = 4741;
+
+app.get("/", async (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, '/index.html'))
+});
 
 app.get("/posts", async (req, res) => {
   const fetchRes = await fetch("https://jsonplaceholder.typicode.com/posts");
   const data = await fetchRes.json();
-  console.log(data);
+//   console.log(data);
   res.status(200).json(data);
 });
 
 app.get("/posts/:id", async (req, res) => {
   const fetchRes = await fetch(`https://jsonplaceholder.typicode.com/posts/${req.params.id}`);
   const data = await fetchRes.json();
-  console.log(data);
+//   console.log(data);
   res.status(200).json(data);
 });
 
