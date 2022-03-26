cdfecfaaec3286f273e5e37249bfadc019f5a837
Kevin J Mosley <kevinjmosley93@gmail.com>
2022-03-25 18:52:12 -0700

    add blog, projects and skills routes

--- server.js
+++ server.js
@@ -11,6 +11,24 @@ app.get("/", async (req, res) => {
   res.status(200).sendFile(path.join(__dirname, '/index.html'))
 });
 
+app.get("/blog", async (req, res) => {
+  const fetchRes = await fetch("https://employthevets.com/api/get-blog");
+  const data = await fetchRes.json();
+//   console.log(data);
+  res.status(200).json(data);
+});
+app.get("/projects", async (req, res) => {
+  const fetchRes = await fetch("https://kevinjmosley.com/api/get-projects");
+  const data = await fetchRes.json();
+//   console.log(data);
+  res.status(200).json(data);
+});
+app.get("/skills", async (req, res) => {
+  const fetchRes = await fetch("https://kevinjmosley.com/api/get-skilss");
+  const data = await fetchRes.json();
+//   console.log(data);
+  res.status(200).json(data);
+});
 app.get("/todos", async (req, res) => {
   const fetchRes = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=5");
   const data = await fetchRes.json();
