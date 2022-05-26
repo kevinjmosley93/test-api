9e3473539d790da6ad1316b5cbb13f39f6533483
Kevin J Mosley <Kevinjmosley93@gmail.com>
2022-05-26 16:26:11 -0700

    change

--- server.js
+++ server.js
@@ -1,68 +1,94 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 const express = require("express");
-const path = require('path')
-const cors = require('cors')
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
 const app = express();
 const port = 4741;
 
-app.use(cors())
+app.use(cors());
+app.use(bodyParser.json()); // for parsing application/json
 
 app.get("/", async (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, '/index.html'))
+  res.status(200).sendFile(path.join(__dirname, "/index.html"));
 });
 
 app.get("/blog", async (req, res) => {
   const fetchRes = await fetch("https://employthevets.com/api/get-blog");
   const data = await fetchRes.json();
-//   console.log(data);
+  //   console.log(data);
   res.status(200).json(data);
 });
 app.get("/projects", async (req, res) => {
   const fetchRes = await fetch("https://kevinjmosley.com/api/get-projects");
   const data = await fetchRes.json();
-//   console.log(data);
+  //   console.log(data);
   res.status(200).json(data);
 });
 app.get("/skills", async (req, res) => {
   const fetchRes = await fetch("https://kevinjmosley.com/api/get-skills");
   const data = await fetchRes.json();
-//   console.log(data);
+  //   console.log(data);
   res.status(200).json(data);
 });
+
 app.get("/todos", async (req, res) => {
-  const fetchRes = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=5");
+  const fetchRes = await fetch(
+    "https://jsonplaceholder.typicode.com/todos?_limit=5"
+  );
   const data = await fetchRes.json();
-//   console.log(data);
+  //   console.log(data);
   res.status(200).json(data);
 });
 
 app.get("/todos/:id", async (req, res) => {
-  const fetchRes = await fetch(`https://jsonplaceholder.typicode.com/todos/${req.params.id}`);
+  const fetchRes = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${req.params.id}`
+  );
   const data = await fetchRes.json();
-//   console.log(data);
+  //   console.log(data);
   res.status(200).json(data);
 });
 
 app.get("/users", async (req, res) => {
-  const fetchRes = await fetch("https://jsonplaceholder.typicode.com/users?_limit=5");
+  const fetchRes = await fetch(
+    "https://jsonplaceholder.typicode.com/users?_limit=5"
+  );
   const data = await fetchRes.json();
-//   console.log(data);
+  //   console.log(data);
   res.status(200).json(data);
 });
 
 app.get("/users/:id", async (req, res) => {
-  const fetchRes = await fetch(`https://jsonplaceholder.typicode.com/users/${req.params.id}`);
+  const fetchRes = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${req.params.id}`
+  );
   const data = await fetchRes.json();
-//   console.log(data);
+  //   console.log(data);
   res.status(200).json(data);
 });
 app.get("/users/:id/todos", async (req, res) => {
-  const fetchRes = await fetch(`https://jsonplaceholder.typicode.com/users/${req.params.id}/todos?_limit=5`);
+  const fetchRes = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${req.params.id}/todos?_limit=5`
+  );
   const data = await fetchRes.json();
-//   console.log(data);
+  //   console.log(data);
   res.status(200).json(data);
 });
+app.post("/agency", async (req, res) => {
+  const { name, email, phone, subject, message } = await req.body;
 
+  const data = {
+    name,
+    email,
+    phone,
+    subject,
+    message,
+  };
+  console.log("data:", data);
+  res.status(200).json(data);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
